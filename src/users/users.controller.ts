b6d79d3aa41bb7647e716f0115de1d217bdd5664
ahmed elselly <ahmedelselly87@gmail.com>
2026-02-06 16:52:39 +0200

    added interceptor for user to exclude the password of user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
 import {
   Body,
+  ClassSerializerInterceptor,
   Controller,
   Delete,
   Get,
@@ -7,6 +8,7 @@ import {
   Patch,
   Post,
   Query,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
@@ -22,6 +24,7 @@ export class UsersController {
     this.userService.create(email, password);
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(parseInt(id));
